0ec896b35ccc698dd5bf82080ef99a02fd644347
Daniel Vilas <danielvilasgonzalez@gmail.com>
2026-09-09 10:25:40 +0200

    update dirs

--- scripts/01_survey_density_custom.R
+++ scripts/01_survey_density_custom.R
@@ -112,7 +112,7 @@ if (tolower(Sys.info()[["user"]]) == "daniel" && .Platform$OS.type == "unix") {
   )
   
   pcloud_dir <- rstudioapi::selectDirectory()
-  if (is.null(out_dir) || out_dir == "" || !dir.exists(out_dir)) {
+  if (is.null(pcloud_dir) || pcloud_dir == "" || !dir.exists(pcloud_dir)) {
     stop("No valid pcloud directory selected.")
   }
   
@@ -129,7 +129,7 @@ if (tolower(Sys.info()[["user"]]) == "daniel" && .Platform$OS.type == "unix") {
     )
   )
   git_dir <- rstudioapi::selectDirectory()
-  if (is.null(out_dir) || out_dir == "" || !dir.exists(out_dir)) {
+  if (is.null(git_dir) || git_dir == "" || !dir.exists(git_dir)) {
     stop("No valid Github directory selected.")
   }
 }
@@ -848,3 +848,4 @@ finalize_workbook_sheet_order(
 )
 
 message("\nDone. Outputs in ", out_dir, " and ", plot_dir)
+
